f6a234d6b9454532093e0f6b0149f4137fa9c9fe
Stefan Kruik <80580843+SVKruik@users.noreply.github.com>
2024-09-13 10:17:43 +0200

    URL Fix
    SK Platform was moved from the bots subdomain to it's own dedicated
    subdomain.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,9 +10,8 @@ export default defineComponent({
     <div class="flex-col">
         <h1>Stefan Kruik - Portfolio</h1>
         <p>I am still working on it, and as of now I have not published anything yet.</p>
-        <p>Currently I am focussing on <a href="https://bots.stefankruik.com/" target="_blank">SK Platform</a>, another
-            big
-            project of mine.</p>
+        <p>Currently I am focussing on <a href="https://platform.stefankruik.com/" target="_blank">SK Platform</a>, another
+            big project of mine.</p>
         <p>The portfolio website is open source, and you can find the GitHub repository <a
                 href="https://github.com/SVKruik/Portfolio-Website" target="_blank">here</a>.</p>
     </div>
